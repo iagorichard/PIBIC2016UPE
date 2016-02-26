daa5981bad3a9e40dff53ada8634133479decf78
iagorichard <iagorrs@gmail.com>
2016-02-26 14:39:39 -0300

    Arquivos de simulação de cenários adicionados.

---
+++ CloudSim/src/runTests/Cenario2.java
@@ -0,0 +1,15 @@
+/**
+ * Este é o nosso segundo arquivo de simulação.
+ * Aqui iremos simular um cenário de 4 hosts, 4 VM e 1 Cloudlet.
+ * Métrica a ser utiliada: Modelagem da taxa de chegada de clientes.
+ */
+package runTests;
+
+/**
+ * Classe principal da simulação.
+ * @author Iago Silva
+ * @author Leylane Ferreira
+ */
+public class Cenario2 {
+    
+}
